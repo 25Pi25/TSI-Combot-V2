233d54334259b7f53209e5bef17350b1be7e0732
25Pi25 <25pi25mail@gmail.com>
2026-02-21 21:58:21 -0500

    added option for lucky 7 toggle

--- src/commands/util/lucky7.ts
+++ src/commands/util/lucky7.ts
@@ -4,7 +4,11 @@ import { db, lucky7Toggles } from '../../util';
 
 export const description = new SlashCommandBuilder()
   .setName("lucky7")
-  .setDescription("Toggles lucky 7 for the specified user.");
+  .setDescription("Toggles lucky 7 for the specified user.")
+  .addBooleanOption(option => option
+    .setName("toggle")
+    .setDescription("Toggle the option on/off directly.")
+  );
 
 export interface Lucky7 {
   user: string
@@ -12,8 +16,9 @@ export interface Lucky7 {
 }
 
 export default async function (interaction: ChatInputCommandInteraction) {
+  const toggle = interaction.options.getBoolean("toggle");
   const currentSettings = db.prepare("SELECT toggle FROM L7 WHERE user = ?").get(interaction.user.id) as Lucky7 | undefined;
-  const newSetting = currentSettings ? +!currentSettings.toggle : 1;
+  const newSetting = +(toggle ?? (currentSettings ? currentSettings.toggle : true));
   db.prepare("INSERT INTO L7 VALUES (?, ?) ON CONFLICT(user) DO UPDATE SET toggle = ?").run(interaction.user.id, newSetting, newSetting);
   lucky7Toggles[interaction.user.id] = newSetting;
   interaction.reply({ content: `Lucky 7 ${newSetting ? "enabled" : "disabled"}!`, flags: 'Ephemeral' });
